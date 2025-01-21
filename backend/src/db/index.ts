88a580d7df5db280307191763e42d8d72d82935a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-01-21 19:15:40 +0100

    feat: implement turso

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,17 +1,6 @@
-import Database from 'better-sqlite3';
-import { DB_FILE_PATH, SCHEMA_FILE_PATH } from '../constants';
-import { readFile } from 'fs/promises';
+import { DB_FILE_PATH } from '../constants';
+import { createClient } from '@libsql/client';
 
-const db = new Database(DB_FILE_PATH);
-
-(async () => {
-  try {
-    const schemaQuery = await readFile(SCHEMA_FILE_PATH, 'utf-8');
-
-    db.exec(schemaQuery);
-  } catch (_error) {
-    console.error('There was an error trying to create the database table');
-  }
-})();
+const db = createClient({ url: `file:${DB_FILE_PATH}` });
 
 export default db;
