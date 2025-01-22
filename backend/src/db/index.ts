5cb4a2aa26df6e5ecec546e6835d763e48cfcf40
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-01-22 07:53:56 +0100

    feat(database): use local or remote database based on NODE_ENV variable

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,6 +1,17 @@
 import { DB_FILE_PATH } from '../constants';
 import { createClient } from '@libsql/client';
+import 'dotenv/config';
+import HttpError from '../errors/HttpError';
 
-const db = createClient({ url: `file:${DB_FILE_PATH}` });
+const { NODE_ENV, TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env;
+
+const IS_PRODUCTION = NODE_ENV === 'prod';
+
+const url = IS_PRODUCTION ? TURSO_DATABASE_URL : `file:${DB_FILE_PATH}`;
+
+if (null == url)
+  throw new HttpError({ message: 'Database URL is not properly configured', status: 500 });
+
+const db = createClient({ url, authToken: TURSO_AUTH_TOKEN });
 
 export default db;
